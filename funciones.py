ec122808642d11b48e39023f76eea6b2a33230c9
Antonio Miguel <amiguel@unizar.es>
2022-07-07 02:23:14 +0200

    .

--- funciones.py
+++ funciones.py
@@ -49,37 +49,53 @@ def ver_datos(x, cols, y):
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.naive_bayes import MultinomialNB
-# from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import LogisticRegression
 
 def entrenar_clasificador1(train, labels):
     vec = CountVectorizer(max_features= 1000, preprocessor=limpiar_texto)    
     x = vec.fit_transform(train)
-    # print(vec.get_feature_names_out())
-    # x = x.toarray()
     model = MultinomialNB().fit(x, labels)
-    # model = LogisticRegression().fit(x.astype(np.float32), labels)
     return model, vec
 
 def evaluar_clasificador1(model, vec, texto):
     x = vec.transform([texto])
-    # x = x.toarray().astype(np.float32)
     return model.predict(x)
 
 def entrenar_clasificador2(train, labels):
     vec = CountVectorizer(max_features= 1000, preprocessor=limpiar_texto)    
     x = vec.fit_transform(train)
-    # print(vec.get_feature_names_out())  
     tfidf = TfidfTransformer()
     x = tfidf.fit_transform(x)
-    # x = x.toarray()
-    # y = np.array(labels)
     model = MultinomialNB().fit(x, labels)
-    # model = LogisticRegression().fit(x.astype(np.float32), y)
     return model, vec, tfidf
     
 def evaluar_clasificador2(model, vec, tfidf, texto):
     x = vec.transform([texto])
     x = tfidf.transform(x)
-    # x = x.toarray().astype(np.float32)
     return model.predict(x)
 
+def word2vec_linea(word2vec, line):
+    x = [word2vec[x] for x in line.split() if x in word2vec] 
+    return sum(x)/len(x)
+
+def entrenar_clasificador3(word2vec, train, labels):
+    train = [ limpiar_texto(x) for x in train ]
+    train = [ word2vec_linea(word2vec,linea) for linea in train]
+    x = np.array(train)
+    model = LogisticRegression().fit(x, labels)
+    return model
+
+def evaluar_clasificador3(word2vec, model, texto):
+    train = [ limpiar_texto(texto) ]
+    train = [ word2vec_linea(word2vec,linea) for linea in train]
+    x = np.array(train)
+    return model.predict(x)
+
+
+from sklearn.cluster import KMeans
+def clustering1(word2vec, textos, numero_clusters):
+    textos = [ limpiar_texto(x) for x in textos ]
+    textos = [ word2vec_linea(word2vec,linea) for linea in textos]
+    x = np.array(textos)
+    kmeans = KMeans(n_clusters=numero_clusters, random_state=0).fit(x)
+    return kmeans.labels_
